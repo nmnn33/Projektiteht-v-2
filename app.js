4ff2e6f087c0c9a373938a93f0a65bff9c8713a7
nmnn33 <tommy123tran@gmail.com>
2022-03-26 22:24:36 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+//Minun itse tuomat moduulit, muuttujat jne..
+const PORT = process.env.PORT || 8083; //Portti
+var http = require('https');
+var fs = require('fs');
+// express generatorista valmiiksi tulleet moduulit
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+//sisältävät polku logiikan javascriptillä
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express(); //express juttumme
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.locals.pretty = true;
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Polut
+app.use('/', indexRouter); //basic polku
+app.use('/users', usersRouter); //turha??
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//Port listener
+app.listen(PORT, function () {
+  console.log("Kuunnellaan porttia: " + PORT);
+});
---
+++ routes/index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+
+//to render, tässä on mun API taulukkoon tiedot
+let data = {};
+var otsikko = [];
+var urli = [];
+var paivays = [];
+var kuva = [];
+data = { otsikko, urli, paivays, kuva };
+
+//NY times API
+const request = require('request');
+
+function execute() {
+  const options = {
+    "url": "https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?api-key=tjdZEwcQu7yR4jJTxF2JYT9Iw7PaoPVq",
+    "method": "GET",
+    "headers": {
+      "Accept": "application/json"
+    }
+  };
+  request(options, function (err, res, body) {
+    if (err) {
+      console.error(err);
+    }
+    else {
+      var json = JSON.parse(body);
+      for (var i = 0; i < json.results.length; i++) {
+        otsikko.push(
+          json.results[i].title
+        );
+        urli.push(
+          json.results[i].url
+        );
+        paivays.push(
+          json.results[i].published_date
+        );
+        kuva.push(
+          json.results[i].media[0]['media-metadata'][2].url
+        );
+      }
+      console.log(data);
+    }
+  });
+}
+
+execute(); //Aloittaa ja Päättää mun API
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', data);
+});
+
+module.exports = router;
